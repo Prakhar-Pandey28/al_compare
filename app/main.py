c6214dbc9064b44684bb63ad78bfb9dc5e8ca4c2
Prakhar Pandey <prakharpandey2811@gmail.com>
2025-12-04 12:54:58 +0530

    Wire Extractor Into main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,14 @@ async def read_root():
 async def compare_pdfs(pdf_a: UploadFile = File(...),
                        pdf_b: UploadFile = File(...)):
     
-    # Read file Objects
+    """
+    Endpoint to compare two PDFs.
+    V0: we only parse them into sections and return the raw sections.
+    Later we will add similarity + pharma logic.
+    """
+ 
+    # Use our extractor on the underlying file-like object (.file)
+    # Note: extract_pdf_sections expects a file-like object, not UploadFile directly.
 
     a_sections = extract_pdf_sections(pdf_a.file)
     b_sections = extract_pdf_sections(pdf_b.file)
